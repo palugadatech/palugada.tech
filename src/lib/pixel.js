9299ab08348767adad8839d9084d454f35c17c19
palugada.tech <palugadatech.official@gmail.com>
2026-05-19 20:32:34 +0700

    edit pixel

--- src/lib/pixel.js
+++ src/lib/pixel.js
@@ -1,16 +1,17 @@
 import ReactPixel from 'react-facebook-pixel';
 
 export const trackWhatsAppClick = (location = 'unknown') => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && window.fbq) {
     ReactPixel.track('Contact', {
       content_name: 'WhatsApp Redirect',
       content_category: 'Lead Generation',
       location: location
     });
     
-    // Also track as custom event for more specific data
     ReactPixel.trackCustom('WhatsAppClick', {
       location: location
     });
+  } else {
+    console.log("Meta Pixel tidak aktif atau diblokir browser. Event dilewati.");
   }
 };
